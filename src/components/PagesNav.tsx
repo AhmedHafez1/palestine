34d5374ca975656356676710bf5c84974ec2e50b
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-10-12 21:11:04 +0300

    3

--- src/components/PagesNav.tsx
+++ src/components/PagesNav.tsx
@@ -5,7 +5,11 @@ export default function PagesNav() {
     <ul>
       <li>
         <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
         <NavLink to="/product">Product</NavLink>
+      </li>
+      <li>
         <NavLink to="/pricing">Pricing</NavLink>
       </li>
     </ul>
