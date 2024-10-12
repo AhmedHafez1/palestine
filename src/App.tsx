64a4a0105b6bc8f399d103f540a19cee8845fe57
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-10-12 20:42:41 +0300

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
 import Product from './pages/Product';
 import Home from './pages/Home';
 import Pricing from './pages/Pricing';
+import PagesNav from './components/PagesNav';
 
 function App() {
   return (
     <BrowserRouter>
       <h1>Palestine</h1>
+      <PagesNav></PagesNav>
       <Routes>
         <Route path="" element={<Home />}></Route>
         <Route path="product" element={<Product />}></Route>
